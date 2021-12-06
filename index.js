d71904e603461b9c5a3c2ebd00008fae67ed68c7
Robert Edmonds <bobby.edmonds89@gmail.com>
2021-12-05 19:45:40 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(array, string){
+    const pickDriver = array.filter(name => name.toUpperCase() === string.toUpperCase())
+    return pickDriver
+}
+function fuzzyMatch(array, string){
+    const pickDriverByLetter = array.filter(char => char.slice(0, string.length) === string);
+    return pickDriverByLetter
+}
+function matchName(array, string){
+    const whereDrivers = array.filter(name=> name.name === string)
+    return whereDrivers
+}
+    
